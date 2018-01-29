947a46658a706211a8fb6235b5dd616be9afc209
Raúl Rodríguez Cruces <raulrcruces@gmail.com>
2018-01-29 16:39:20 -0600

    wav visualization

--- wav.R
+++ wav.R
@@ -1,2 +1,11 @@
 # Library for wav reading
 library(tuneR)
+
+# list all files from a directory ls
+files <- list.files("/misc/ernst/rcruces/git_here/2018_music_psychophysics/files/")
+
+# Read a wav mono file
+wav1 <- readWave("/misc/ernst/rcruces/git_here/2018_music_psychophysics/files/01.3sec.wav")
+
+# Counts the number of points of the file
+N <- length(wav1@left)
